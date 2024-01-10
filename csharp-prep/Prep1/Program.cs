02af93e59083255771f36224b7166702e26e0b2c
coleblancett <danielblancett@gmail.com>
2024-01-10 11:13:17 -0700

    Made Prep1 Prettier

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,8 +6,10 @@ class Program
     {
         Console.WriteLine("What is your first name? ");
         string first = Console.ReadLine();
+
         Console.WriteLine("What is your last name? ");
         string last = Console.ReadLine();
+        
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
