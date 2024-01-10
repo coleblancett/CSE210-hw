4393d975be65ecfe9f33caca3aa6797ec2f5b66a
coleblancett <danielblancett@gmail.com>
2024-01-10 14:46:18 -0700

    Completed Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello! What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int letter = int.Parse(grade);
+
+        if (letter >= 90)
+        {
+            Console.Write("You got an A!");
+        }
+        else if (letter < 90 && letter >= 80)
+        {
+            Console.Write("You got a B!");
+        }
+        else if (letter < 80 && letter >= 70)
+        {
+            Console.Write("You got a C!");
+        }
+        else if (letter < 70 && letter >= 60)
+        {
+            Console.Write("You got a D!");
+        }
+        else if (letter < 60)
+        {
+            Console.Write("You got an F!");
+        }
     }
 }
