57dba6fee2963d45c812875df355e8802886a157
coleblancett <danielblancett@gmail.com>
2024-01-17 10:34:05 -0700

    Begin Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        int magicNumber = Console.ReadLine()
+
+        Console.WriteLine("What is your guess? ");
+        int guess = Console.ReadLine();
+
+        if guess > magicNumber {
+            console.WriteLine("Lower");
+        }
+
+        elseif guess < magicNumber {
+            console.WriteLine("Higher");
+        }
+
+        else guess == magicNumber {
+            console.WriteLine("You guessed it! ");
+        }
     }
 }
