d7907e400a9e1cdc7c0717a9f7cd2c65491ae6ad
coleblancett <danielblancett@gmail.com>
2024-01-17 17:22:24 -0700

    Prep5 Completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome(){
+            Console.WriteLine("Welcome to the Program! ");
+        }
+
+        static string PromptUserName(){
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber(){
+            Console.WriteLine("Please enter your favorite number: ");
+            int userNum = int.Parse(Console.ReadLine());
+            return userNum;
+        }
+
+        static int SquareNumber(int userNum){
+            int squared = userNum * userNum;
+            return squared;
+        }
+
+        static void DisplayResult(int squared, string userName){
+            Console.WriteLine($"{userName}, the sqaure of your number is {squared}");
+        }
+
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNum = SquareNumber(userNumber);
+
+        DisplayResult(squaredNum, name);
     }
 }
