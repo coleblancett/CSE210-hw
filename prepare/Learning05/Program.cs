e74228585e7efbc3e735fc174ebfca4108a56e5c
coleblancett <danielblancett@gmail.com>
2024-03-04 09:57:08 -0700

    Finish Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+        Square s = new("red", 5);
+        Rectangle r = new("white", 3, 4);
+        Circle c = new("blue", 10);
+
+        foreach (Shape i in shapes)
+        {
+            string color = i.GetColor();
+            double area = i.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
