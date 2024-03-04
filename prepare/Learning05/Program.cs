f3d15fc3379779d23dc5fc60d377dc09aa6aed5a
coleblancett <danielblancett@gmail.com>
2024-03-04 09:59:26 -0700

    Need to add shapes to list

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -8,8 +8,11 @@ class Program
     {
         List<Shape> shapes = new();
         Square s = new("red", 5);
+        shapes.Add(s);
         Rectangle r = new("white", 3, 4);
+        shapes.Add(r);
         Circle c = new("blue", 10);
+        shapes.Add(c);
 
         foreach (Shape i in shapes)
         {
